321b73f7c23ce72ceed302365f5650782e011a59
abmish <needabhinav@gmail.com>
2016-03-13 21:23:19 +0530

    e20 sol

---
+++ euler/e20.py
@@ -0,0 +1,17 @@
+"""
+n! means n x (n - 1) x ... x 3 x 2 x 1
+For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+def sum_of_digits(n):
+    s = 0
+    while n > 0:
+        s = s + (n % 10)
+        n = n / 10
+    return s
+
+n = 1
+for i in xrange(1,100): n = n*i
+print sum_of_digits(n)
